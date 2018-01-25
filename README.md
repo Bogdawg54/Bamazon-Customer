355ac48c72e6d1a1f02bc3724d3ba488bb189cee
Graham Bass <gbass@lec.edu>
2018-01-25 11:48:51 -0500

    4th time's the charm

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Bamazon-Customer
+# bamazon
+ Amazon-like storefront with mySQL.User will be shown the existing products and can buy items.
+
+# Node
+
+*  mysql
+*  inquirer
+
+First install or update your local project's **npm** tools:
+
+
+# First install all the NPM tools:
+npm install
+```
+# Start the app
+
+
+node bamazonCustomer.js
+```
+
+If database connection is successful, you should see
+
+connected as id 
+```
+
+And then the inventory list
+
+# Using the app
+
+Type in the id of the item you want to buy. In the above example that would be 1-11
+
+Once you have chosen an item, type the number of units you would like
+
+If there is enough inventory you should see "Succesfully bought for $[total]"
+
+otherwise you will get
+```node
+Insufficient quantity
+```
+
+In both cases, the app restart and prompt you for what you would like to buy again.
+ 
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,83 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3307,
+
+    
+    user: "root",
+
+    
+    password: "root",
+    database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    console.log("\n");
+    initialize();
+});
+
+function initialize() {
+    console.log("\nHere are all our products today!\n");
+    connection.query(
+        "SELECT * FROM products",
+        function(error, res) {
+            if (error) throw error;
+            
+            res.forEach(function(element, index, array) {
+                console.log(element.item_id + ". " + element.product_name + " - " 
+                	+ element.department_name + " - Price: $" + element.price + "\n");
+            })
+            console.log("---------------------------");
+
+            inquirer
+                .prompt([{
+                        name: "id",
+                        type: "input",
+                        message: "What item id would you like to buy?"
+                    },
+                    {
+                        name: "quantity",
+                        type: "input",
+                        message: "How many units?"
+                    }
+                ])
+                .then(function(answer) {
+                    connection.query(
+                        "SELECT * FROM products WHERE item_id=?", [answer.id],
+                        function(error, res) {
+                            if (error) throw error;
+
+                            var stockQuantity = res[0].stock_quantity;
+                            var userUnitRequest = parseInt(answer.quantity);
+                            var price = res[0].price;
+                            var newQuantity = stockQuantity - userUnitRequest;
+                            if (stockQuantity >= userUnitRequest) {
+                                updateQuantity(answer.id, newQuantity,userUnitRequest, price);
+                            }
+                            else{
+                            	console.log("Insufficient quantity");
+                            	initialize();
+                            }
+                        }
+                    )
+                });
+        }
+    )
+}
+
+function updateQuantity(id, quantity, units, price) {
+    connection.query(
+        "UPDATE products SET stock_quantity=? WHERE item_id=?", [quantity, id],
+        function(error, res) {
+            if (error) throw error;
+            var total = units * price;
+            console.log("Successfully bought for $" + total);
+            initialize();
+        }
+    )
+    
+}
